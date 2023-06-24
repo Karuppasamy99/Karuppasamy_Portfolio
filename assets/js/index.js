a0ba038f828f129c2198fd17340fd5781b98629e
Karuppasamy99 <karuppasamy.a@hotmail.com>
2023-06-24 19:14:50 +0530

    mail changes

--- assets/js/index.js
+++ assets/js/index.js
@@ -25,3 +25,23 @@ function toggleActive(){
 }
 
 navLink.forEach( n => n.addEventListener('click',toggleActive));
+
+const sendMail = (e) => {
+    e.preventDefault()
+    const msg = document.getElementById('message')
+    const email = document.getElementById('email')
+    
+    Email.send({
+        SecureToken : "dc5312c3-8410-4343-94b1-42523c63115d",
+        To : "jagdishkannan0@gmail.com",
+        From : "jagdishkannan0@gmail.com",
+        Subject : "PortFolio",
+        Body : email.value +"/n"+ msg.value,
+    }).then(
+      message => alert(message)
+    );
+}
+
+const form = document.querySelector('.contact_form')
+form.addEventListener('submit',sendMail)
+
