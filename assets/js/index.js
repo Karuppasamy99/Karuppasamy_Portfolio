44830a5338771540b09faa8893b9d47f2873df1a
Karuppasamy99 <karuppasamy.a@hotmail.com>
2023-07-18 17:34:18 +0530

    portfolio_completed

--- assets/js/index.js
+++ assets/js/index.js
@@ -26,22 +26,22 @@ function toggleActive(){
 
 navLink.forEach( n => n.addEventListener('click',toggleActive));
 
-const sendMail = (e) => {
-    e.preventDefault()
-    const msg = document.getElementById('message')
-    const email = document.getElementById('email')
+// const sendMail = (e) => {
+//     e.preventDefault()
+//     const msg = document.getElementById('message')
+//     const email = document.getElementById('email')
     
-    Email.send({
-        SecureToken : "dc5312c3-8410-4343-94b1-42523c63115d",
-        To : "jagdishkannan0@gmail.com",
-        From : "jagdishkannan0@gmail.com",
-        Subject : "PortFolio",
-        Body : email.value +"  "+ msg.value,
-    }).then(
-      message => alert(message)
-    );
-}
+//     Email.send({
+//         SecureToken : "dc5312c3-8410-4343-94b1-42523c63115d",
+//         To : "jagdishkannan0@gmail.com",
+//         From : "jagdishkannan0@gmail.com",
+//         Subject : "PortFolio",
+//         Body : email.value +"  "+ msg.value,
+//     }).then(
+//       message => alert(message)
+//     );
+// }
 
-const form = document.querySelector('.contact_form')
-form.addEventListener('submit',sendMail)
+// const form = document.querySelector('.contact_form')
+// form.addEventListener('submit',sendMail)
 
